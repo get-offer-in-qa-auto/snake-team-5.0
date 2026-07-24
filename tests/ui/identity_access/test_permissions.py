7890deb192bad77af893691cedda99ed972578d0
nik54 <nik54a@gmail.com>
2026-07-24 16:00:24 +0300

    ui tests

--- tests/ui/identity_access/test_permissions.py
+++ tests/ui/identity_access/test_permissions.py
@@ -9,4 +9,4 @@ from src.main.ui.pages.project_page import ProjectPage
 @pytest.mark.regression
 @pytest.mark.user_session("project_viewer_user")
 def test_project_viewer_cannot_create_entities(page: Page, project: ProjectResponse):
-    (ProjectPage(page, project).open().should_be_opened().should_not_allow_creation())
+    ProjectPage(page, project).open().should_be_opened().should_not_allow_creation()
